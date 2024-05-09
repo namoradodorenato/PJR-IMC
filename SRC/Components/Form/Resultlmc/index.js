b749e147daa4b544cab6efcc7f9ff9e05ac37de3
namoradodorenato <rhuanzitcher@gmail.com>
2024-05-09 10:40:03 -0300

    Resultado

---
+++ SRC/Components/Form/Resultlmc/index.js
@@ -0,0 +1,11 @@
+import react from "react";
+import {View, Text} from "reactie-native";
+
+export default function ResultImc(props){
+    return (
+        <View> 
+            <Text>{props.messageResultImc}</Text>
+            <Text>{props.ResultImc}</Text>
+        </View>
+    );
+}
